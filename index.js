2f2593afcf43a2ac6be7e811aceaef3a725119cc
Greg Morgan <gregm9999@gmail.com>
2019-01-13 03:22:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ class BoardMember {
   }
 
   doCharity() {
-    return "I like to help people."
+    return "I like to help people.";
+  }
+  releasePressStatement() {
+    return "You will see great things from Scuber.";
   }
 }
